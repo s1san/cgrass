6fa11de376b28723c92cec611e58c1e39df40e4f
s1san <s1sanCN@163.com>
2024-01-21 17:43:42 +0800

    finish json-tutorial01_answer

--- leptjson.h
+++ leptjson.h
@@ -22,7 +22,7 @@ enum
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SIGULAR
+    LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
 int lept_parse(lept_value *v, const char *json);
