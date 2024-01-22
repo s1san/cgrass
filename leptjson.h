a5258022162a3f522a9bc54cfa8802cb94d4b82c
s1san <s1sanCN@163.com>
2024-01-22 15:30:32 +0800

    finish json-toturial02_answer

--- leptjson.h
+++ leptjson.h
@@ -14,6 +14,7 @@ typedef enum
 
 typedef struct
 {
+    double n;
     lept_type type;
 } lept_value;
 
@@ -22,11 +23,14 @@ enum
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_ROOT_NOT_SINGULAR,
+    LEPT_PARSE_NUMBER_TOO_BIG
 };
 
 int lept_parse(lept_value *v, const char *json);
 
 lept_type lept_get_type(const lept_value *v);
+
+double lept_get_number(const lept_value *v);
 
 #endif // LEPTJSON_H__
